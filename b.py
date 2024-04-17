a2baeb2b8045926ff4926e402d179d90c6a88bef
Adriel Mounzón <adriel.mounzon@ucb.edu.bo>
2024-04-17 18:40:09 -0400

    Inicio de nuevo mover

---
+++ b.py
@@ -0,0 +1,9 @@
+import numpy
+
+matriz = numpy.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+p = slice(None)
+
+a = matriz[p,0]
+
+print(a)
--- prueba.py
+++ prueba.py
@@ -32,125 +32,133 @@ class Cubo:
             capa.imprimir()
             print()
 
-
-    def  Unuevo(self):
-         self.Mover(["F", "L", "B", "R"], "U", 3)
-
-    def U(self):
-            capas_afectadas = ["F", "L", "B", "R"] 
-            capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[0])
-            for i in range(0,3):
-                self.capas[capas_afectadas[3-i]].get_caras()[0] = self.capas[capas_afectadas[2-i]].get_caras()[0]
-            self.capas[capas_afectadas[0]].get_caras()[0] = capa
-            self.capas["U"].get_caras()[:] = numpy.rot90(self.capas["U"].get_caras(), k=3)
-
-    def Mover(self, capas_afectadas, capa_rotada, k):
-        capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[0])
+    def Mover(self, capas_afectadas, filas, columnas, inversos, capa_rotada, k):
+        columnas = [slice(None) if col == ':' else col for col in columnas]
+        filas = [slice(None) if fil == ':' else fil for fil in filas]
+        capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[filas[3], columnas[3]][::inversos[3]])
         for i in range(0,3):
-            self.capas[capas_afectadas[3-i]].get_caras()[0] = self.capas[capas_afectadas[2-i]].get_caras()[0]
-        self.capas[capas_afectadas[0]].get_caras()[0] = capa
+            self.capas[capas_afectadas[3-i]].get_caras()[filas[3-i], columnas[3-i]] = self.capas[capas_afectadas[2-i]].get_caras()[filas[2-i], columnas[2-i]][::inversos[2-i]]
+        self.capas[capas_afectadas[0]].get_caras()[filas[2-i], columnas[2-i]] = capa
         self.capas[capa_rotada].get_caras()[:] = numpy.rot90(self.capas[capa_rotada].get_caras(), k=k)
 
+    def MoverColumna2(self, capas_afectadas, filas, columnas, inversos, capa_rotada, k):
+        self.Mover(capas_afectadas, filas, columnas, inversos, capa_rotada, k)
+
+    def MoverFila(self, capas_afectadas, capa_rotada, k, fila):
+        capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[fila])
+        for i in range(0,3):
+            self.capas[capas_afectadas[3-i]].get_caras()[fila] = self.capas[capas_afectadas[2-i]].get_caras()[fila]
+        self.capas[capas_afectadas[0]].get_caras()[fila] = capa
+        self.capas[capa_rotada].get_caras()[:] = numpy.rot90(self.capas[capa_rotada].get_caras(), k=k)
+
+    def MoverColumna(self, capas_afectadas, capa_rotada, k, columna):
+        capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[:,columna])
+        for i in range(0,3):
+            if capas_afectadas[2-i]=="B":
+                self.capas[capas_afectadas[3-i]].get_caras()[:,columna] = self.capas[capas_afectadas[2-i]].get_caras()[:,(columna==0)*2][::-1]
+            else:
+                self.capas[capas_afectadas[3-i]].get_caras()[:,columna] = self.capas[capas_afectadas[2-i]].get_caras()[:,columna]
+        self.capas[capas_afectadas[0]].get_caras()[:,columna] = capa
+        self.capas[capa_rotada].get_caras()[:] = numpy.rot90(self.capas[capa_rotada].get_caras(), k=k)
+
+    def MoverFilaColumna(self, capas_afectadas, posiciones, capa_rotada, k):
+        f4=copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[posiciones[3]])
+        self.capas[capas_afectadas[3]].get_caras()[posiciones[3]] = self.capas[capas_afectadas[2]].get_caras()[:,posiciones[2]][::-1]
+        self.capas[capas_afectadas[2]].get_caras()[:,posiciones[2]] = self.capas[capas_afectadas[1]].get_caras()[posiciones[1]]
+        self.capas[capas_afectadas[1]].get_caras()[posiciones[1]] = self.capas[capas_afectadas[0]].get_caras()[:,posiciones[0]][::-1]
+        self.capas[capas_afectadas[0]].get_caras()[:,posiciones[0]] = f4
+        self.capas[capa_rotada].get_caras()[:] = numpy.rot90(self.capas[capa_rotada].get_caras(), k=k)
+
+    def MoverFilaColumnaInverso(self, capas_afectadas, posiciones, capa_rotada, k):
+        f4=copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[posiciones[3]])
+        self.capas[capas_afectadas[3]].get_caras()[posiciones[3]] = self.capas[capas_afectadas[2]].get_caras()[:,posiciones[2]]
+        self.capas[capas_afectadas[2]].get_caras()[:,posiciones[2]] = self.capas[capas_afectadas[1]].get_caras()[posiciones[1]][::-1]
+        self.capas[capas_afectadas[1]].get_caras()[posiciones[1]] = self.capas[capas_afectadas[0]].get_caras()[:,posiciones[0]]
+        self.capas[capas_afectadas[0]].get_caras()[:,posiciones[0]] = f4[::-1]
+        self.capas[capa_rotada].get_caras()[:] = numpy.rot90(self.capas[capa_rotada].get_caras(), k=k)
+
+
+    def  U(self):
+         self.MoverFila(["F", "L", "B", "R"], "U", 3, 0)  
+
     def Up(self):
-            capas_afectadas = ["F", "R", "B", "L"] 
-            capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[0])
-            for i in range(0,3):
-                self.capas[capas_afectadas[3-i]].get_caras()[0] = self.capas[capas_afectadas[2-i]].get_caras()[0]
-            self.capas[capas_afectadas[0]].get_caras()[0] = capa
-            self.capas["U"].get_caras()[:] = numpy.rot90(self.capas["U"].get_caras(), k=1)
+        self.MoverFila(["F", "R", "B", "L"], "U", 1, 0)
 
     def D(self):
-            capas_afectadas = ["F", "R", "B", "L"] 
-            capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[2])
-            for i in range(0,3):
-                self.capas[capas_afectadas[3-i]].get_caras()[2] = self.capas[capas_afectadas[2-i]].get_caras()[2]
-            self.capas[capas_afectadas[0]].get_caras()[2] = capa
-            self.capas["D"].get_caras()[:] = numpy.rot90(self.capas["D"].get_caras(), k=3)
+        self.MoverFila(["F", "R", "B", "L"], "D", 3, 2)
 
     def Dp(self):
-            capas_afectadas = ["F", "L", "B", "R"] 
-            capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[2])
-            for i in range(0,3):
-                self.capas[capas_afectadas[3-i]].get_caras()[2] = self.capas[capas_afectadas[2-i]].get_caras()[2]
-            self.capas[capas_afectadas[0]].get_caras()[2] = capa
-            self.capas["D"].get_caras()[:] = numpy.rot90(self.capas["D"].get_caras(), k=1)  
+        self.MoverFila(["F", "L", "B", "R"], "D", 1, 2)
 
     def L(self):
-            capas_afectadas = ["F", "D", "B", "U"]
-            capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[:,0])
-            for i in range(0,3):
-                self.capas[capas_afectadas[3-i]].get_caras()[:,0] = self.capas[capas_afectadas[2-i]].get_caras()[:,0]
-            self.capas[capas_afectadas[0]].get_caras()[:,0] = capa
-            self.capas["L"].get_caras()[:] = numpy.rot90(self.capas["L"].get_caras(), k=3)
+        self.MoverColumna(["F", "D", "B", "U"], "L", 3, 0)
+
+    def L2(self):
+        self.MoverColumna2(["F", "D", "B", "U"], [":", ":", ":", ":"], [0, 0, 2, 0], [1, 1, -1, 1], "L", 3)
 
     def Lp(self):
-            capas_afectadas = ["F", "U", "B", "D"]
-            capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[:,0])
-            for i in range(0,3):
-                self.capas[capas_afectadas[3-i]].get_caras()[:,0] = self.capas[capas_afectadas[2-i]].get_caras()[:,0]
-            self.capas[capas_afectadas[0]].get_caras()[:,0] = capa
-            self.capas["L"].get_caras()[:] = numpy.rot90(self.capas["L"].get_caras(), k=1)
+        self.MoverColumna(["F", "U", "B", "D"], "L", 1, 0)
     
     def R(self):
-            capas_afectadas = ["F", "U", "B", "D"]
-            capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[:,2])
-            for i in range(0,3):
-                self.capas[capas_afectadas[3-i]].get_caras()[:,2] = self.capas[capas_afectadas[2-i]].get_caras()[:,2]
-            self.capas[capas_afectadas[0]].get_caras()[:,2] = capa
-            self.capas["R"].get_caras()[:] = numpy.rot90(self.capas["R"].get_caras(), k=3)
+        self.MoverColumna(["F", "U", "B", "D"], "R", 3, 2)
 
     def Rp(self):
-            capas_afectadas = ["F", "D", "B", "U"]
-            capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[:,2])
-            for i in range(0,3):
-                self.capas[capas_afectadas[3-i]].get_caras()[:,2] = self.capas[capas_afectadas[2-i]].get_caras()[:,2]
-            self.capas[capas_afectadas[0]].get_caras()[:,2] = capa
-            self.capas["R"].get_caras()[:] = numpy.rot90(self.capas["R"].get_caras(), k=1)
+        self.MoverColumna(["F", "D", "B", "U"], "R", 1, 2)
 
     def F(self):
-            capas_afectadas = ["L", "U", "R", "D"]
-            capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[:,2])
-            for i in range(0,3):
-                self.capas[capas_afectadas[3-i]].get_caras()[:,2] = self.capas[capas_afectadas[2-i]].get_caras()[:,2]
-            self.capas[capas_afectadas[0]].get_caras()[:,2] = capa
-            self.capas["F"].get_caras()[:] = numpy.rot90(self.capas["F"].get_caras(), k=3)
+        self.MoverFilaColumna(["L", "U", "R", "D"], [2, 2, 0, 0], "F", 3)
 
-    #Probar a partir de aqui
     def Fp(self):
-            capas_afectadas = ["L", "D", "R", "U"]
-            capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[:,2])
-            for i in range(0,3):
-                self.capas[capas_afectadas[3-i]].get_caras()[:,2] = self.capas[capas_afectadas[2-i]].get_caras()[:,2]
-            self.capas[capas_afectadas[0]].get_caras()[:,2] = capa
-            self.capas["F"].get_caras()[:] = numpy.rot90(self.capas["F"].get_caras(), k=1)
+        self.MoverFilaColumnaInverso(["L", "D", "R", "U"], [2, 0, 0, 2], "F", 1)
 
     def B(self):
-            capas_afectadas = ["L", "U", "R", "D"]
-            capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[:,0])
-            for i in range(0,3):
-                self.capas[capas_afectadas[3-i]].get_caras()[:,0] = self.capas[capas_afectadas[2-i]].get_caras()[:,0]
-            self.capas[capas_afectadas[0]].get_caras()[:,0] = capa
-            self.capas["B"].get_caras()[:] = numpy.rot90(self.capas["B"].get_caras(), k=3)
+        self.MoverFilaColumnaInverso(["L", "D", "R", "U"], [0, 2, 2, 0], "B", 3)
 
     def Bp(self):
-            capas_afectadas = ["L", "D", "R", "U"]
-            capa = copy.deepcopy(self.capas[capas_afectadas[3]].get_caras()[:,0])
-            for i in range(0,3):
-                self.capas[capas_afectadas[3-i]].get_caras()[:,0] = self.capas[capas_afectadas[2-i]].get_caras()[:,0]
-            self.capas[capas_afectadas[0]].get_caras()[:,0] = capa
-            self.capas["B"].get_caras()[:] = numpy.rot90(self.capas["B"].get_caras(), k=1)
+        self.MoverFilaColumna(["L", "U", "R", "D"], [0, 0, 2, 2], "B", 1)
     
 
 
-capa1 = Capa("B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9")
-capa2 = Capa("R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9")
+capa1 = Capa("G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9")
+capa2 = Capa("O1", "O2", "O3", "O4", "O5", "O6", "O7", "O8", "O9")
 capa3 = Capa("W1", "W2", "W3", "W4", "W5", "W6", "W7", "W8", "W9")
-capa4 = Capa("O1", "O2", "O3", "O4", "O5", "O6", "O7", "O8", "O9")
+capa4 = Capa("R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9")
 capa5 = Capa("Y1", "Y2", "Y3", "Y4", "Y5", "Y6", "Y7", "Y8", "Y9")
-capa6 = Capa("G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9")
+capa6 = Capa("B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9")
 
 cubo = Cubo(capa1, capa2, capa3, capa4, capa5, capa6)
 
-cubo.Unuevo()
+# cubo.R()
+# cubo.R()
+# cubo.L()
+# cubo.L()
+# cubo.F()
+# cubo.F()
+# cubo.B()
+# cubo.B()
+# cubo.U()
+# cubo.U()
+# cubo.D()
+# cubo.D()
+
+cubo.L2()
+
+
+# cubo.R()
+# cubo.U()
+# cubo.Fp()
+# cubo.Lp()
+# cubo.B()
+# cubo.D()
+# cubo.Rp()
+# cubo.F()
+# cubo.U()
+# cubo.Bp()
+# cubo.D()
+# cubo.L()
+# cubo.Up()
+# cubo.Fp()
+# cubo.R()
+
 
 cubo.imprimir()
